4fd5820dbe6c348dba3f5acf605c8e72904aa60e
Will Cade <chakka2112@gmail.com>
2023-04-29 13:56:19 -0600

    broot force

---
+++ bruteForce.py
@@ -0,0 +1,17 @@
+from maestro import Controller
+import time 
+
+MOTORS = 1
+TURN = 2
+BODY = 0
+HEADTILT = 4
+HEADTURN = 3
+
+tango = Controller()
+
+motors = 6700
+turn = 5300
+
+for x in range (200):
+    tango.setTarget(MOTORS,motors)
+    tango.setTarget(TURN, turn)
