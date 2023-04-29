c0ff286a3a4f9f92f944c63ad16fc51f83c71748
Will Cade <chakka2112@gmail.com>
2023-04-29 15:10:22 -0600

    oooo fugg a gougar :DDD

--- bruteForce.py
+++ bruteForce.py
@@ -11,10 +11,10 @@ tango = Controller()
 
 motors = 6700
 turn = 5300
+tango.setTarget(MOTORS,motors)
+tango.setTarget(TURN, turn)
 
-for x in range (200):
-    tango.setTarget(MOTORS,motors)
-    tango.setTarget(TURN, turn)
+
 
 motors = 6000
 turn = 6000
